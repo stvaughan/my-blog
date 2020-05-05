d84798c3f4bb82dbdb0fd9a9afc5e8b53e1242a5
Shaunda Vaughan <stv@scarletmail.rutgers.edu>
2020-05-05 15:56:50 -0400

    First Commit

---
+++ src/build/precache-manifest.5991cbd3b93913564d23936d5c88188a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b25629c6b80d0bf8b4915e0b22154a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e43e2daa965b8336dc",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "e9b31015f404169a4f93",
+    "url": "/static/js/2.e741812d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e741812d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e43e2daa965b8336dc",
+    "url": "/static/js/main.c70a3330.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
